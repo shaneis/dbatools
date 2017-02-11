833c9525c0e82d6ee8f25840c57c4a3701df43fe
Shaneis <shaneoneillis@gmail.com>
2017-02-11 22:38:47 +0000

    cleaning up changes

--- internal/Connect-SqlServer.ps1
+++ internal/Connect-SqlServer.ps1
@@ -1,4 +1,4 @@
-Function Connect-SqlServer
+﻿Function Connect-SqlServer
 {
 <# 
 .SYNOPSIS 
@@ -20,7 +20,6 @@ Internal function that creates SMO server object. Input can be text or SMO.Serve
 		if ($ParameterConnection)
 		{
 			$paramserver = New-Object Microsoft.SqlServer.Management.Smo.Server
-			$paramserver.ConnectionContext.ConnectTimeout = 2
 			$paramserver.ConnectionContext.ApplicationName = "dbatools PowerShell module - dbatools.io"
 			$paramserver.ConnectionContext.ConnectionString = $SqlServer.ConnectionContext.ConnectionString
 			
@@ -57,8 +56,28 @@ Internal function that creates SMO server object. Input can be text or SMO.Serve
 		return $SqlServer
 	}
 	
+	# This seems a little complex but is required because some connections do TCP,sqlserver
+	[regex]$portdetection = ":\d{1,5}$"
+	if ($sqlserver.LastIndexOf(":") -ne -1)
+	{
+		$portnumber = $sqlserver.substring($sqlserver.LastIndexOf(":"))
+		if ($portnumber -match $portdetection)
+		{
+			$replacedportseparator = $portnumber -replace ":", ","
+			$sqlserver = $sqlserver -replace $portnumber, $replacedportseparator
+		}
+	}
+	
 	$server = New-Object Microsoft.SqlServer.Management.Smo.Server $SqlServer
 	$server.ConnectionContext.ApplicationName = "dbatools PowerShell module - dbatools.io"
+
+    if ($server.Configuration.SmoAndDmoXPsEnabled.RunValue -eq 0)
+    {
+        Write-Error "Accessing this server via SQL Management Objects (SMO) or Distributed Management Objects (DMO) is currently not permitted.
+                     Enable the option 'SMO and DMO XPs' on your instance using sp_configure to continue.
+                     Note that this will require 'Show Advanced Options' to be enabled using sp_configure as well."
+        break
+    }
 	
 	try
 	{
@@ -90,11 +109,7 @@ Internal function that creates SMO server object. Input can be text or SMO.Serve
 	{
 		if ($ParameterConnection)
 		{
-			$server.ConnectionContext.ConnectTimeout = 2
-		}
-		else
-		{
-			$server.ConnectionContext.ConnectTimeout = 3
+			$server.ConnectionContext.ConnectTimeout = 7
 		}
 		
 		$server.ConnectionContext.Connect()
